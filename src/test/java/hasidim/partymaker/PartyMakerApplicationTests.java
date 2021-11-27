57b4773a84798f327c8800506b9c3358a15ce6b0
Aios <qufrha@gmail.com>
2021-11-27 19:47:55 +0900

    project init

---
+++ src/test/java/hasidim/partymaker/PartyMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package hasidim.partymaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
